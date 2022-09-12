f7ad499fdb71480f1d8a52f552bc4fd0bd309ab4
sihun <thrchtlgns@naver.com>
2022-09-12 18:59:51 +0900

    [chore] config패키지 설치

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+  getUser(): string {
+    return this.configService.get('SECRET');
   }
 }
