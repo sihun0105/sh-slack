04e59353d205884eb9e58209ccfe0e6ffd1fc3fe
sihun <thrchtlgns@naver.com>
2022-09-17 20:51:44 +0900

    [feat] main.ts Swagger추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  
   console.log(`listen on port ${port}`);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Sleact API개발을 위한 API문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(port);
 }
 bootstrap();
