5e4fc43e19a85402664a5da23dfce35691429e46
sihun <thrchtlgns@naver.com>
2022-09-20 17:37:20 +0900

    [feat] Users Entity 데코레이트작업

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -21,15 +22,31 @@ import { Workspaces } from './Workspaces';
 @Index('email', ['email'], { unique: true })
 @Entity({ schema: 'sleact', name: 'users' })
 export class Users {
+  @ApiProperty({
+    example : '1',
+    description : '사용자 아이디'
+  })
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
-
+  
+  @ApiProperty({
+    example : 'tlgns14@nate.com',
+    description : '사용자 이메일'
+  })
   @Column('varchar', { name: 'email', unique: true, length: 30 })
   email: string;
 
+  @ApiProperty({
+    example : '김시후니후니',
+    description : '사용자 닉네임'
+  })
   @Column('varchar', { name: 'nickname', length: 30 })
   nickname: string;
 
+  @ApiProperty({
+    example : '1q2w3e4r',
+    description : '사용자 비밀번호'
+  })
   @Column('varchar', { name: 'password', length: 100, select: false })
   password: string;
 
