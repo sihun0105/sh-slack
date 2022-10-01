9aca439b2dca7c720c185eb15bb48796cdac7301
sihun <thrchtlgns@naver.com>
2022-10-01 11:43:22 +0900

    [feat] workspace서비스 DI

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { Channels } from 'src/entities/Channels';
+import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { Workspaces } from 'src/entities/Workspaces';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspaceRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMemberRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMemberRepository: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private UsersRepository: Repository<Users>,
+  ) {}
+}
